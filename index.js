734d405e5d858f562e29ac5185dc74c1452de811
xcstream <cheng.xu@all-dream.com>
2019-03-20 11:26:08 +0800

    1

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const serveStatic = require('serve-static')
 const static = serveStatic('public')
 const bodyParser = require("body-parser");
 
+const util = require('util')
+const exec = util.promisify(require('child_process').exec);
+
 app = express()
 app.use(bodyParser.json({ type: 'application/json',limit:'50mb' }))
 app.use(bodyParser.urlencoded({ extended: false,limit:'50mb' }));
@@ -16,13 +19,18 @@ app.use('/pdfon/create',async (req, res) => {
 
     var url = req.query.url;
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    var p = 'pdfon/pdf/' + new Date().getTime()+''+parseInt( Math.random()*1000000)+'.pdf'
+    var p = new Date().getTime()+''+parseInt( Math.random()*1000000)+'.pdf'
     const page = await browser.newPage();
     await page.goto(url);
-    await page.pdf({path: 'public/'+p, format: 'A4'});
+    await page.pdf({path: '/tmp/'+p, format: 'A4'});
+
+    var cmd= (`ossutil cp ${'/tmp/'+p} oss://aomengassets/upup/files/`)
+    console.log(cmd)
+    exec(cmd)
+
     await browser.close();
 
-    res.send({code:200,url:p})
+    res.send({code:200,url:'https://cdn.all-dream.com/upup/files/'+p})
 })
 
 // app.use('/',(req,res)=>{
